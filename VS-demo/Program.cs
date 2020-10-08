2daa25b5315cbcb912658d8385c7df963a3a3c35
MikeZorin <mykhailo.zorin@gmail.com>
2020-10-08 14:50:13 -0500

    added lesson 4

--- VS-demo/Program.cs
+++ VS-demo/Program.cs
@@ -1,132 +1,149 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace VS_demo
 {
-    enum Importance
-    {
-        None,
-        Trivial,
-        Regular,
-        Important,
-        Critical
-    }
-
-    public delegate int Operation(int a, int b);
-    public delegate void PrintMsg();
-
     class Program
     {
         static void Main(string[] args)
         {
-            Importance value = Importance.Critical;
+            /*
+            try
+            {
+                // ctrl + K + X
+                //int i = 0;
+                //int res = 10 / i;
+                int i = 100;
 
-            if (value == Importance.Critical)
-                Console.WriteLine("Very important");
-
-            // Generic
-            bool b1 = isEqual("9", "9");
-            bool b2 = isEqual(10.9, 10.9);
-
-            Console.WriteLine(b1 ? "equal" : "not equal");
-            Console.WriteLine(b2 ? "equal" : "not equal");
-
-            // String builder
-            string path = "C:";
-
-            path = path + "\\Csharp";
-            path = path + "\\Lessons";
-            path += "\\Lessons-001.cs";
-
-            Console.WriteLine(path);
-
-            StringBuilder sb = new StringBuilder("C:");
-
-            sb.Append("\\Csharp").Append("\\Lessons").Append("\\Lessons-001.cs");
-
-            Console.WriteLine(sb);
-
-            StringBuilder sb2 = new StringBuilder("This is an example string that is an example");
-            // an to the
-            sb2.Replace("an", "the");
-
-            Console.WriteLine(sb2);
-
-            StringBuilder sb3 = new StringBuilder();
-
-            sb3.AppendFormat("Hello {0}. This is {1}", "Mike", "Friday", "Monday");
-            sb3.AppendLine();
-            sb3.AppendLine();
-            sb3.AppendLine();
-            sb3.AppendLine();
-            sb3.Append("New Line");
-            Console.WriteLine(sb3[0]);
-
-            // join(', ') // one, two, three
-            string[] elems = { "bird", "cat", "dog" };
-            StringBuilder sb4 = new StringBuilder();
-
-            sb4.AppendJoin(", ", elems); // *
-
-            Console.WriteLine(sb4);
-
-            // delegates
-
-            int res = 0;
-
-            res = Run(new Operation(Add), 22, 44);
-
-            Console.WriteLine("res = {0}", res);
+                var anw1 = Console.ReadLine();
 
 
-            Talk(new PrintMsg(Hello));
-            Talk(new PrintMsg(Bye));
+
+                if (i < 200)
+                {
+                    throw new FormatException("Input str is....");
+                    throw new ArgumentOutOfRangeException("Value less then 200");
+                }
+
+                StreamReader sr = new StreamReader("d:\\test\\data.txt");
+
+                Console.WriteLine(sr.ReadToEnd());
+
+                sr.Close();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Yahhhh!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+
+            Console.WriteLine("There is a big block code!!!!");
+            */
+
+
+            List<string> list = new List<string>(2);
+
+            list.Add("001");
+            list.Add("002");
+            list.Add("003"); // i = 2, 002.5
+            list.Add("004");
+            list.Add("005");
+            list.Add("006");
+            list.Add("007");
+            list.Add("008");
+            list.Add("009");
+
+            Console.WriteLine( list.Count + " " + list.Capacity );
+            list[4] = "0044";
+            Console.WriteLine(list[4]);
+
+            
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-----------------------------------------");
+
+            list.Insert(2, "002.5");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-----------------------------------------");
+
+            Console.WriteLine(list.IndexOf("00800")); // -1
+
+            Console.WriteLine(list.Contains("007")); // -1
+
+            // Predicate 
+
+            Console.WriteLine(list.Exists(s => s == "0077"));
+            Console.WriteLine("-----------------------------------------");
+
+            List<string> names = new List<string>();
+
+            names.Add("Bob");
+            names.Add("Mark");
+            names.Add("Steve");
+            names.Add("Mike");
+
+
+            List<string> names2 = new List<string>();
+
+            names2.Add("Lana");
+            names2.Add("Mary");
+            names2.Add("Tom");
+
+            names.InsertRange(3, names2);
+
+            Console.WriteLine(names.Find(n => n.StartsWith('M')));
+            Console.WriteLine(names.FindLast(n => n.StartsWith('M')));
+
+            var filteredNames = names.FindAll(n => n.StartsWith('M'));
+
+            // names.Remove("Tom");
+            names.RemoveAll(n => n.StartsWith('M'));
+
+
+            Console.WriteLine("-----------------------------------------");
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-----------------------------------------");
+
+            string[] namesArr = names.ToArray();
+
+            for (int i = 0; i < namesArr.Length; i++)
+            {
+                Console.WriteLine(namesArr[i]);
+            }
+            Console.WriteLine("-----------------------------------------");
+
+            List<string> l_copy = namesArr.ToList();
+
+            foreach (var item in l_copy)
+            {
+                Console.WriteLine(item);
+            }
         }
 
-        static int Add(int a, int b)
-        {
-            return a + b;
-        }
-
-        static void Talk(PrintMsg pm)
-        {
-            pm.Invoke(); // pm()
-        }
-
-        static void Hello()
-        {
-            Console.WriteLine("Hello!");
-        }
-
-        static void Bye()
-        {
-            Console.WriteLine("Bye!");
-        }
-
-        static int Subt(int a, int b)
-        {
-            return a - b;
-        }
-
-        static int Mult(int a, int b)
-        {
-            return a * b;
-        }
-
-        static int Run(Operation act, int a, int b)
-        {
-            return act(a, b);
-        }
-
-        static bool isEqual<T>(T a, T b)
-        {
-            return a.Equals(b); // ==
-        }
-
-        static bool isEqual2(object a, object b)
-        {
-            return a.Equals(b); // ==
-        }
     }
 }
 
